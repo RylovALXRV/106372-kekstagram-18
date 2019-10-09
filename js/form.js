cf3a20a1f1b23359d2a9fa530b5bb40126ab0bbc
Алексей <rylovalex2108l@mail.ru>
2019-10-09 08:40:39 +0700

    Выполнил задание №15. Личный проект: воздушные формы

--- js/form.js
+++ js/form.js
@@ -3,21 +3,10 @@
 (function () {
   var picturesElement = document.querySelector('.pictures');
   var descriptionFieldElement = picturesElement.querySelector('.text__description');
+  var formElement = picturesElement.querySelector('.img-upload__form');
   var hashtagInputElement = picturesElement.querySelector('.text__hashtags');
   var imgOverlayElement = picturesElement.querySelector('.img-upload__overlay');
-  var imgPreviewElement = picturesElement.querySelector('.img-upload__preview img');
-  var uploadCancelElement = picturesElement.querySelector('.img-upload__cancel');
-  var uploadFileElement = picturesElement.querySelector('#upload-file');
   var inputChecked = picturesElement.querySelector('.effects__list input[checked]');
-
-  var resetForm = function () {
-    descriptionFieldElement.textContent = '';
-    hashtagInputElement.value = '';
-    imgPreviewElement.style.transform = '';
-    imgPreviewElement.style.filter = '';
-    imgPreviewElement.className = '';
-    uploadFileElement.value = '';
-  };
 
   var setDefaultValuesForm = function () {
     document.querySelector('.scale .scale__control--value').value = '100%';
@@ -29,21 +18,33 @@
     if (evt.keyCode === window.util.ESC_KEYCODE &&
       evt.target !== hashtagInputElement &&
       evt.target !== descriptionFieldElement) {
-      resetForm();
+      window.modal.resetForm();
 
       window.util.closePopup(imgOverlayElement, previewCloseKeydownHandler);
     }
   };
 
-  uploadFileElement.addEventListener('change', function () {
+  var closeSuccessFormSubmition = function () {
+    imgOverlayElement.classList.add('hidden');
+
+    window.modal.showSuccess();
+    window.modal.resetForm();
+  };
+
+  picturesElement.querySelector('#upload-file').addEventListener('change', function () {
     setDefaultValuesForm();
 
     window.util.openPopup(imgOverlayElement, previewCloseKeydownHandler);
   });
 
-  uploadCancelElement.addEventListener('click', function () {
-    resetForm();
+  picturesElement.querySelector('.img-upload__cancel').addEventListener('click', function () {
+    window.modal.resetForm();
 
     window.util.closePopup(imgOverlayElement, previewCloseKeydownHandler);
+  });
+
+  formElement.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(formElement), closeSuccessFormSubmition, window.modal.showError);
+    evt.preventDefault();
   });
 })();
--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,37 @@
 'use strict';
 
 (function () {
+
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var successTemplate = document.querySelector('#success').content.querySelector('.success');
+  var imgOverlayElement = document.querySelector('.img-upload__overlay');
   var mainElement = document.querySelector('main');
+  var picturesElement = document.querySelector('.pictures');
+  var descriptionFieldElement = picturesElement.querySelector('.text__description');
+  var hashtagInputElement = picturesElement.querySelector('.text__hashtags');
+  var imgPreviewElement = picturesElement.querySelector('.img-upload__preview img');
+  var uploadFileElement = picturesElement.querySelector('#upload-file');
+
+  // Перенес ф-ию из form.js для того, чтобы сбрасывать стили в окне с ошибкой,
+  // при нажатии на кнопку Загрузить другой файл
+  var resetForm = function () {
+    descriptionFieldElement.value = '';
+    hashtagInputElement.value = '';
+    imgPreviewElement.style.transform = '';
+    imgPreviewElement.style.filter = '';
+    imgPreviewElement.className = '';
+    uploadFileElement.value = '';
+  };
 
   var errorCloseKeydownHandler = function (evt) {
     if (evt.keyCode === window.util.ESC_KEYCODE) {
       deletePopup(mainElement.querySelector('.error'), errorCloseKeydownHandler);
+    }
+  };
+
+  var successCloseKeydownHandler = function (evt) {
+    if (evt.keyCode === window.util.ESC_KEYCODE) {
+      deletePopup(mainElement.querySelector('.success'), successCloseKeydownHandler);
     }
   };
 
@@ -16,12 +41,21 @@
     document.removeEventListener('keydown', callback);
   };
 
-  var removeModal = function (modalElement, button) {
+  var loadErrorFile = function (modalElement, callback) {
+    imgOverlayElement.classList.add('hidden');
+    deletePopup(modalElement, callback);
+    resetForm();
+  };
+
+  var removeModal = function (modalElement, callback) {
     modalElement.addEventListener('click', function (evt) {
       var element = document.elementFromPoint(evt.clientX, evt.clientY);
 
-      if (modalElement === element || button === evt.target) {
-        deletePopup(modalElement, errorCloseKeydownHandler);
+      if (modalElement === element || element.classList.contains('error__button--try') ||
+        element.classList.contains('success__button')) {
+        deletePopup(modalElement, callback);
+      } else if (element.classList.contains('error__button--load')) {
+        loadErrorFile(modalElement, callback);
       }
     });
   };
@@ -31,16 +65,31 @@
     var errorButtons = errorElement.querySelectorAll('.error__button');
 
     errorElement.querySelector('.error__title').textContent = errorMessage;
-    errorButtons[0].style.margin = '0px';
-    errorButtons[1].classList.add('hidden');
 
     document.addEventListener('keydown', errorCloseKeydownHandler);
-    removeModal(errorElement, errorButtons[0]);
+    removeModal(errorElement, errorCloseKeydownHandler);
+
+    // Если окно загрузки фотографии закрыто, значит окно с ошибкой содержит одну кнопку
+    if (imgOverlayElement.classList.contains('hidden')) {
+      errorButtons[0].style.margin = '0px';
+      errorButtons[1].classList.add('hidden');
+    }
 
     mainElement.appendChild(errorElement);
   };
 
+  var showSuccess = function () {
+    var successElement = successTemplate.cloneNode(true);
+
+    document.addEventListener('keydown', successCloseKeydownHandler);
+    removeModal(successElement, successCloseKeydownHandler);
+
+    mainElement.appendChild(successElement);
+  };
+
   window.modal = {
-    showError: showError
+    resetForm: resetForm,
+    showError: showError,
+    showSuccess: showSuccess
   };
 })();
